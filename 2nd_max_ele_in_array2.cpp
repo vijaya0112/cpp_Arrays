617f2571b192df057c16e09e992c5792d940a280
vijaya0112 <69664353+vijaya0112@users.noreply.github.com>
2021-05-28 16:54:34 +0530

    Add files via upload

---
+++ 2nd_max_ele_in_array2.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int n;
+	int arr[n];
+	cout<<"Enter n value:";
+	cin>>n;
+	cout<"Enter array elements:";
+	for(int i=0;i<n;i++)
+	{
+		cin>>arr[i];
+	}
+	int val1=INT_MIN;
+	int val2=INT_MIN;
+	val1=arr[0];
+	for(int i=0;i<n;i++)
+	{
+		if(arr[i]>val1)
+		{
+			val2=val1;
+			val1=arr[i];
+		}
+		else
+		{
+			if(val2<arr[i])
+			{
+				val2=arr[i];
+			}
+		}
+	}
+	cout<<val2;
+	
+}
